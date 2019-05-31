07841f194cc51706c99a1f01517f5d704aae3ff3
Mark Pahor <mark@Marks-MBP.socal.rr.com>
2019-05-30 17:43:29 -0700

    js functions to add new topic buttons to array done. js function to pull and display gifs and ratings done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,27 +3,45 @@ var moods = ["sad", "angry", "happy", "confused", "calm", "excited"]
 
 //create display function to display gifs in html - will call this function later for when a mood button is clicked
 function displayGIF () {
+    //clear gif div necessary? will see once I can get gifs to show - yep necessary
+    $("#gifs-view").empty()
 
+    //make sure I'm pulling the q from the data attribute
     var mood = $(this).attr("data-word")
     console.log(mood)
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + mood + "&api_key=k8hbtr9mNHZjp3GDaC6g0DADF0rSouaD&limit=10"
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + mood + "&api_key=k8hbtr9mNHZjp3GDaC6g0DADF0rSouaD&limit=10"
     //create ajax call with a limit of 10
     $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
         console.log(response)
+    //for loope to iterate through each gif in the response array
+    for (var j= 0; j<response.data.length; j++) {
 
-    //create a variable to store the gif itself (embed url?)
-        var gif = response.data
+    //create new div to store gif and rating
+    var newDiv = $("<div>");
+    
+    //create new div to store gif itself
+     var gifImg = $("<img>");
+
     //create an element to display the rating
+    newDiv.append("<p>Rating: " + response.data[j].rating + "</p>")
 
-    //create a variable to store the rating
+    //pull gif attributes to use on click events on gifImg
+    gifImg.attr({
+        "src": response.data[j].images.original_still.url,
+        "data-still": response.data[j].images.original_still.url,
+        "data-animate": response.data[j].images.original.url,
+        "data-state": "still",
+        "class": "gif"
+    });
 
-    //create an element to display the rating 
+    newDiv.append(gifImg);
 
     //append gif and rating to #gifs-view
-    $("#gifs-view").append(gif)
+    $("#gifs-view").append(newDiv)
+};
     });
 };
 
@@ -51,14 +69,19 @@ function renderButtons() {
     };
 
 //create on click event for when a new mood is submitted
-$("#add-gif").click(function(event) {
+$("#add-gif").on("click", function(event) {
     event.preventDefault();
+    console.log("add-gif click event working")
     
-    //create a variable that pulls value from input box
+    //create a variable that pulls value from input box 
+    var newMood = $("#keyword-input").val().trim();
+    console.log(newMood)
 
     //push value to moods array
+    moods.push(newMood);
 
     //call render buttons function to create button for new mood
+    renderButtons();
 
 
 })
@@ -66,6 +89,6 @@ $("#add-gif").click(function(event) {
 
 // Calling the renderButtons function to display the intial buttons
 renderButtons();
-//call display function with on click even on mood buttons (class of mood defined in render buttons fx)
+//call display function with on click event on mood buttons (class of mood defined in render buttons fx)
 $(document).on("click", ".mood", displayGIF);
 
